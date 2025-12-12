874cec02926bef3df0079b9e2786ffa9d100e57f
JeromeA-greta <jerome.anamoutou@gmail.com>
2025-12-12 16:34:30 +0100

    Adaptation du code pour Windows

--- shorty.py
+++ shorty.py
@@ -6,22 +6,47 @@ from platformdirs import *
 import stat
 import os
 import time
+if os.name == "nt" :
+    from win32com.client import Dispatch
 
-# Fonctions<
+# Fonctions et variables globales
+
+home = os.path.expanduser("~")
+chxLaunch = ""
+chxSave = ""
+pathIcon = ""
+nom = ""
+cmdGen = ""
+version = "0.3.0"
 
 def selFichier() : # Sélection du fichier
-    nomFichier = fd.askopenfilename(#initialdir = '/home/'+user+'/bin/',
-                                    initialdir = os.path.expanduser("~")+"/bin/",
-                                    title = 'Fichier...',
-                                    filetypes = (('AppImage', '*.AppImage'),
-                                                 ('Script .sh', '*.sh'),
-                                                 ('Tous types', '*')))
+    chemin = ""
+    if os.name == "posix" : # Linux
+        nomFichier = fd.askopenfilename(initialdir = home+"/bin",
+                                        title = 'Fichier...',
+                                        filetypes = (('AppImage', '*.AppImage'),
+                                                    ('Script .sh', '*.sh'),
+                                                    ('Tous types', '*')))
+    
+    if os.name == "nt" :    # Windows
+        nomFichier = fd.askopenfilename(initialdir = home,
+                                        title = 'Fichier...',
+                                        filetypes = (('Exécutable (.exe)', '*.exe'),
+                                                    ('Tous types', '*')))
     chxExec.configure(text = nomFichier)
 
 def selIcon() : # Sélection de l'image, qui sera traitée pour que sa taille soit 48x48
     global pathIcon
     global chxIcon
-    nomIcon = fd.askopenfilename(initialdir = os.path.expanduser("~")+'/.icons/',
+
+    chemin =""
+    if os.name == "nt" :
+        chemin = "/shorty/icons"
+        
+    if os.name == "posix" :
+        chemin = "/.icons"
+
+    nomIcon = fd.askopenfilename(initialdir = home+chemin,
                                  title = 'Icône...',
                                  filetypes = (('Image PNG', '*.png'),
                                               ('Image JPG', '*.jpg'),
@@ -35,7 +60,7 @@ def selIcon() : # Sélection de l'image, qui sera traitée pour que sa taille so
     pathIcon = nomIcon
     print(pathIcon)
 
-def genererRac() : # La génération du raccourci
+def linGenRac() : # La génération du raccourci Linux
     global nom
     global chxLaunch
     print(chxSave)
@@ -68,6 +93,25 @@ def genererRac() : # La génération du raccourci
     btnNon = Button(chxLaunch, text = "Non", command = nonLanceur)
     btnNon.grid(column = 2, row = 1)
 
+def winGenRac() :
+    global nom, chxExec
+    nom = txtName.get()
+    bureau = user_desktop_dir()
+    nomRac = nom + ".lnk"
+    chemin = os.path.join(bureau, nomRac)
+    cible = chxExec.cget("text")
+    print(cible)
+    dossier = os.path.dirname(cible)
+    print(os.path.dirname(cible))
+    # icone = pathIcon
+
+    shell = Dispatch("WScript.Shell")
+    rac = shell.CreateShortCut(chemin)
+    rac.Targetpath = cible
+    rac.WorkingDirectory = dossier
+    rac.IconLocation = cible
+    rac.save()
+
 def choix(selChx) : # Choix du type, reçoit ledit type en paramètre
     global chxSave
     chxSave = selChx
@@ -77,17 +121,20 @@ def ouiLanceur() : # Si on choisit d'ajouter au lanceur GNOME
     global nom
     nonLanceur()
     pathDesk = user_desktop_dir()+"/"+nom+".desktop"
-    os.system("cp "+pathDesk+" "+os.path.expanduser("~")+"/.local/share/applications/")
+    os.system("cp "+pathDesk+" "+home+"/.local/share/applications/")
     time.sleep(1)
-    os.system("update-desktop-database "+os.path.expanduser("~")+"/.local/share/applications")
+    os.system("update-desktop-database "+home+"/.local/share/applications")
+
     info = Toplevel(root)
     info.title("Information")
-    # info.geometry("150x80")
     info.resizable(False, False)
+
     lblInfo = Label(info, text = "Ajouté au lanceur !")
     lblInfo.grid(column = 0, row = 0)
+
     info.update()
     info.geometry(str(lblInfo.winfo_width())+"x70")
+
     btnInfo = Button(info, text = "Ok", command = info.destroy)
     btnInfo.grid(column = 0, row = 1)
 
@@ -95,39 +142,40 @@ def nonLanceur() :
     global chxLaunch
     chxLaunch.destroy()
 
-home = os.path.expanduser("~")
-# Vérif : ~/.local/share/applicatons existe ? Si non, création
-if not os.path.isdir(home+"/.local/share/applications") :
-    print("Dossier '~/.local/share/applications' non trouvé ou non présent, création")
-    os.mkdir(home+"/.local/share/applications")
+
+# Vérif : ~/.local/share/applicatons existe ? Si non, création (Spécifique à Linux)
+if os.name == "posix" :
+    cmdGen = linGenRac
+    if not os.path.isdir(home+"/.local/share/applications") :
+        print("Dossier '~/.local/share/applications' non trouvé ou non présent, création")
+        os.mkdir(home+"/.local/share/applications")
+# Version Windows de la fonction au-dessus
+if os.name == "nt" :
+    cmdGen = winGenRac
+    if not os.path.isdir(home+"/shorty/icons") :
+        print("Dossier 'shorty' non trouvé ou non présent, création")
+        os.mkdir(home+"/shorty")
+        time.sleep(0.5)
+        os.mkdir(home+"/shorty/icons")
 
 # Création et aménagement de la fenêtre principale
 
 root = Tk()
 root.title("Shorty")
-
-chxLaunch = ""
-chxType = ["Application"] # Tableau contenant les types (Application, URL...)
-chxDefaut = StringVar(root)
-chxDefaut.set(chxType[0]) # Choix par défaut
-chxSave = ""
-pathIcon = ""
-nom = ""
-user = os.getlogin()
-# bureau = os.system("xdg-user-dir DESKTOP")
-# print(os.environ.get('XDG_USER_DIR'))
-
-# root.geometry("300x350")
 root.resizable(False, False)
 
-lblVersion = Label(root, text = "0.2.2") # Numéro de version
+lblVersion = Label(root, text = version) # Numéro de version
 lblVersion.grid(column = 0, row = 0)
 
 # Type
-lblType = Label(root, text = "Type : ")
-lblType.grid(column = 0, row = 1)
-chx = OptionMenu(root, chxDefaut, *chxType, command=choix) # Affichage de la liste des types
-chx.grid(column = 1, row = 1)
+if os.name == "posix" :
+    lblType = Label(root, text = "Type : ")
+    lblType.grid(column = 0, row = 1)
+    chxType = ["Application"] # Tableau contenant les types (Application, URL...)
+    chxDefaut = StringVar(root)
+    chxDefaut.set(chxType[0]) # Choix par défaut
+    chx = OptionMenu(root, chxDefaut, *chxType, command=choix) # Affichage de la liste des types
+    chx.grid(column = 1, row = 1)
 
 # Nom du raccourci souhaité
 lblName = Label(root, text = "Nom : ")
@@ -143,17 +191,18 @@ chxExec.grid(column = 1, row = 3)
 btnExec = Button(root, text = "...", command = selFichier)
 btnExec.grid(column = 2, row = 3)
 
-# Chemin vers l'icône
-lblIcon = Label(root, text = "Icône : ")
-lblIcon.grid(column = 0, row = 4)
-affIcon = ""
-chxIcon = Label(root, image = affIcon)
-chxIcon.grid(column = 1, row = 4)
-btnIcon = Button(root, text="...", command = selIcon)
-btnIcon.grid(column = 2, row = 4)
+# Chemin vers l'icône (Linux)
+if os.name == "posix" :
+    lblIcon = Label(root, text = "Icône : ")
+    lblIcon.grid(column = 0, row = 4)
+    affIcon = ""
+    chxIcon = Label(root, image = affIcon)
+    chxIcon.grid(column = 1, row = 4)
+    btnIcon = Button(root, text="...", command = selIcon)
+    btnIcon.grid(column = 2, row = 4)
 
 # Bouton pour générer le raccourci, avec un choix derrière
-btnGen = Button(root, text = "Générer", command = genererRac)
+btnGen = Button(root, text = "Générer", command = cmdGen)
 btnGen.grid(column=0, row=5)
 
 
